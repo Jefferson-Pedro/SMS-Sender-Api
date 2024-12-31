2aef85cd351f0b97e945fe115fdc8626ca787dd2
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-12-30 23:25:51 -0300

    Implementando novas funcionalidades no Controller

--- SMS-Send-Api/src/main/java/dev/jeff/sms_sender/controller/SmsSenderController.java
+++ SMS-Send-Api/src/main/java/dev/jeff/sms_sender/controller/SmsSenderController.java
@@ -1,23 +1,45 @@
 package dev.jeff.sms_sender.controller;
 
 import dev.jeff.sms_sender.dto.SmsRequest;
-import dev.jeff.sms_sender.service.ISmsSender;
+import dev.jeff.sms_sender.model.Sms;
+import dev.jeff.sms_sender.service.ISmsSenderService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class SmsSenderController {
 
-    private final ISmsSender smsSender;
+    private final ISmsSenderService smsService;
 
-    public SmsSenderController(@Qualifier("twilio") ISmsSender smsSender) {
-        this.smsSender = smsSender;
+    public SmsSenderController(@Qualifier("twilio") ISmsSenderService smsService) {
+        this.smsService = smsService;
     }
 
     @PostMapping("/send-sms")
-    public void sendSms(@RequestBody SmsRequest smsRequest){
-        smsSender.sendSms(smsRequest);
+    public ResponseEntity<String> sendSms(@RequestBody SmsRequest smsRequest){
+
+        smsService.sendSms(smsRequest);
+        return ResponseEntity.ok().body("Mensagem enviada com sucesso!");
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity <List<Sms>> findAll (){
+        List<Sms> list = smsService.findAll();
+        if(!list.isEmpty()){
+            return ResponseEntity.ok(list);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Sms> delete (@PathVariable Integer id){
+
+        if (smsService.delete(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.internalServerError().build();
     }
 }
